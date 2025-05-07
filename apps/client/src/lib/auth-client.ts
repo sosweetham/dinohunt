7d1bd1ac322a5409183b5f17f26d662bc465d20a
Soham Jaiswal <sosweetham@gmail.com>
2025-05-07 09:27:30 +0530

    feat: make api host env var for client

--- apps/client/src/lib/auth-client.ts
+++ apps/client/src/lib/auth-client.ts
@@ -1,10 +1,11 @@
+import { PUBLIC_API_HOST } from "$env/static/public";
 import { createAuthClient } from "better-auth/client";
 import { usernameClient } from "better-auth/client/plugins";
 import { runtime } from "./controllers/runtime.svelte";
 
 export const authClient = createAuthClient({
     /** the base url of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000/api/v1/auth",
+    baseURL: `${PUBLIC_API_HOST}/api/v1/auth`,
     plugins: [usernameClient()],
 });
 
